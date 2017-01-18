5cd7918df180f7a2b6274cce6355a6338b4f44ff
Mauricio Fernandes <mood@gft.com>
2017-01-18 13:41:45 +0100

    rearrange the data in categories

--- app/components/Version4/DealsTableComponent/RefData.js
+++ app/components/Version4/DealsTableComponent/RefData.js
@@ -1,64 +1,148 @@
 export default class RefData {}
 
 RefData.LABELS = [
-  { text: 'Status' },
-  { text: 'Pipeline ID' },
-  { text: 'Degree of Confidence' },
-  { text: 'Termination date' },
-  { text: 'Exposure status' },
-  { text: 'Deal logged' },
-  { text: 'Limits', type: 'section' },
-  { text: '', type: 'limitsComponent' },
-  { text: 'Products', type: 'section' },
-  { text: '', type: 'productsComponent' },
-  { text: 'Counterparties', type: 'section' },
-  { text: '', type: 'counterpartiesComponent' },
-  { text: 'Booking', type: 'section' },
-  { text: '', type: 'bookingComponent' },
-  { text: 'Collateral', type: 'section' },
-  { text: '', type: 'collateralComponent' },
-  { text: 'DE-Risking', type: 'section' },
-  { text: '', type: 'deRiskingComponent' },
+  {
+    main: [
+      { text: 'Status' },
+      { text: 'Pipeline ID' },
+      { text: 'Degree of Confidence' },
+      { text: 'Termination date' },
+      { text: 'Exposure status' },
+      { text: 'Deal logged' },
+    ],
+  },
+  {
+    limits: [
+      { text: 'Limits', type: 'section' },
+      { text: '', type: 'limitsComponent' },
+    ],
+  },
+  {
+    products: [
+      { text: 'Products', type: 'section' },
+      { text: '', type: 'productsComponent' },
+    ],
+  },
+  {
+    counterparties: [
+      { text: 'Counterparties', type: 'section' },
+      { text: '', type: 'counterpartiesComponent' },
+    ],
+  },
+  {
+    booking: [
+      { text: 'Booking', type: 'section' },
+      { text: '', type: 'bookingComponent' },
+    ],
+  },
+  {
+    collateral: [
+      { text: 'Collateral', type: 'section' },
+      { text: '', type: 'collateralComponent' },
+    ],
+  },
+  {
+    derisking: [
+      { text: 'DE-Risking', type: 'section' },
+      { text: '', type: 'deRiskingComponent' },
+    ],
+  },
 ];
 
 RefData.DEAL_LIMITS = [
-  { element: 'select', colSize: 8 },
-  { element: 'text', colSize: 6, optional: true },
-  { element: 'select', colSize: 6 },
-  { element: 'date', colSize: 6 },
-  { element: 'select', colSize: 8 },
-  { element: 'toogleButton', colSize: 12 },
-  { element: '', type: 'section' },
-  { element: 'limitsComponent', type: '' },
-  { element: '', type: 'section' },
-  { element: 'productsComponent', type: '', productsAdded: false },
-  { element: '', type: 'section' },
-  { element: 'counterpartiesComponent', type: '', counterpartyAdded: true },
-  { element: '', type: 'section' },
-  { element: 'bookingComponent', type: '' },
-  { element: '', type: 'section' },
-  { element: 'collateralComponent', type: '' },
-  { element: '', type: 'section' },
-  { element: 'deRiskingComponent', type: '' },
+  {
+    main: [
+      { element: 'select', colSize: 8 },
+      { element: 'text', colSize: 6, optional: true },
+      { element: 'select', colSize: 6 },
+      { element: 'date', colSize: 6 },
+      { element: 'select', colSize: 8 },
+      { element: 'toogleButton', colSize: 12 },
+    ],
+  },
+  {
+    limits: [
+      { element: '', type: 'section' },
+      { element: 'limitsComponent', type: '' },
+    ],
+  },
+  {
+    products: [
+      { element: '', type: 'section' },
+      { element: 'productsComponent', type: '', productsAdded: false },
+    ],
+  },
+  {
+    counterparties: [
+      { element: '', type: 'section' },
+      { element: 'counterpartiesComponent', type: '', counterpartyAdded: true },
+    ],
+  },
+  {
+    booking: [
+      { element: '', type: 'section' },
+      { element: 'bookingComponent', type: '' },
+    ],
+  },
+  {
+    collateral: [
+      { element: '', type: 'section' },
+      { element: 'collateralComponent', type: '' },
+    ],
+  },
+  {
+    derisking: [
+      { element: '', type: 'section' },
+      { element: 'deRiskingComponent', type: '' },
+    ],
+  },
 ];
 
 RefData.DEAL_SUBLIMITS = [
-  { element: 'select', colSize: 8 },
-  { element: 'text', colSize: 6, optional: true },
-  { element: 'select', colSize: 8 },
-  { element: 'date', colSize: 6 },
-  { element: 'select', colSize: 8 },
-  { element: 'toogleButton', colSize: 12 },
-  { element: '', type: 'section' },
-  { element: 'limitsComponent', type: '' },
-  { element: '', type: 'section' },
-  { element: 'productsComponent', type: '', productsAdded: true },
-  { element: '', type: 'section' },
-  { element: 'counterpartiesComponent', type: '', counterpartyAdded: false },
-  { element: '', type: 'section' },
-  { element: 'bookingComponent', type: '' },
-  { element: '', type: 'section' },
-  { element: 'collateralComponent', type: '' },
-  { element: '', type: 'section' },
-  { element: 'deRiskingComponent', type: '' },
+  {
+    main: [
+      { element: 'select', colSize: 8 },
+      { element: 'text', colSize: 6, optional: true },
+      { element: 'select', colSize: 6 },
+      { element: 'date', colSize: 6 },
+      { element: 'select', colSize: 8 },
+      { element: 'toogleButton', colSize: 12 },
+    ],
+  },
+  {
+    limits: [
+      { element: '', type: 'section' },
+      { element: 'limitsComponent', type: '' },
+    ],
+  },
+  {
+    products: [
+      { element: '', type: 'section' },
+      { element: 'productsComponent', type: '', productsAdded: true },
+    ],
+  },
+  {
+    counterparties: [
+      { element: '', type: 'section' },
+      { element: 'counterpartiesComponent', type: '', counterpartyAdded: false },
+    ],
+  },
+  {
+    booking: [
+      { element: '', type: 'section' },
+      { element: 'bookingComponent', type: '' },
+    ],
+  },
+  {
+    collateral: [
+      { element: '', type: 'section' },
+      { element: 'collateralComponent', type: '' },
+    ],
+  },
+  {
+    derisking: [
+      { element: '', type: 'section' },
+      { element: 'deRiskingComponent', type: '' },
+    ],
+  },
 ];
--- app/components/Version4/DealsTableComponent/RowDataFactory.js
+++ app/components/Version4/DealsTableComponent/RowDataFactory.js
@@ -6,27 +6,141 @@ export default class RowDataFactory {
     const rowData = [];
 
     for (let i = 0; i < RefData.LABELS.length; i += 1) {
-      // console.log(RefData.DEAL_LIMITS[i].productsAdded);
-      rowData.push({
-        label: {
-          text: RefData.LABELS[i].text,
-          type: RefData.LABELS[i].type,
-        },
-        deal_limits: {
-          element: RefData.DEAL_LIMITS[i].element,
-          colSize: RefData.DEAL_LIMITS[i].colSize,
-          optional: RefData.DEAL_LIMITS[i].optional,
-          productsAdded: RefData.DEAL_LIMITS[i].productsAdded,
-          counterpartyAdded: RefData.DEAL_LIMITS[i].counterpartyAdded,
-        },
-        deal_sublimits: {
-          element: RefData.DEAL_SUBLIMITS[i].element,
-          colSize: RefData.DEAL_SUBLIMITS[i].colSize,
-          optional: RefData.DEAL_SUBLIMITS[i].optional,
-          productsAdded: RefData.DEAL_SUBLIMITS[i].productsAdded,
-          counterpartyAdded: RefData.DEAL_SUBLIMITS[i].counterpartyAdded,
-        },
-      });
+      switch (Object.keys(RefData.LABELS[i])[0]) {
+        case 'main':
+          for (let j = 0; j < RefData.LABELS[i].main.length; j += 1) {
+            rowData.push({
+              label: {
+                text: RefData.LABELS[i].main[j].text,
+              },
+              deal_limits: {
+                element: RefData.DEAL_LIMITS[i].main[j].element,
+                colSize: RefData.DEAL_LIMITS[i].main[j].colSize,
+                optional: RefData.DEAL_LIMITS[i].main[j].optional,
+              },
+              deal_sublimits: {
+                element: RefData.DEAL_SUBLIMITS[i].main[j].element,
+                colSize: RefData.DEAL_SUBLIMITS[i].main[j].colSize,
+                optional: RefData.DEAL_SUBLIMITS[i].main[j].optional,
+              },
+            });
+          }
+          break;
+        case 'limits':
+          for (let j = 0; j < RefData.LABELS[i].limits.length; j += 1) {
+            rowData.push({
+              label: {
+                text: RefData.LABELS[i].limits[j].text,
+                type: RefData.LABELS[i].limits[j].type,
+              },
+              deal_limits: {
+                element: RefData.DEAL_LIMITS[i].limits[j].element,
+                type: RefData.DEAL_LIMITS[i].limits[j].type,
+              },
+              deal_sublimits: {
+                element: RefData.DEAL_SUBLIMITS[i].limits[j].element,
+                type: RefData.DEAL_SUBLIMITS[i].limits[j].type,
+              },
+            });
+          }
+          break;
+        case 'products':
+          for (let j = 0; j < RefData.LABELS[i].products.length; j += 1) {
+            rowData.push({
+              label: {
+                text: RefData.LABELS[i].products[j].text,
+                type: RefData.LABELS[i].products[j].type,
+              },
+              deal_limits: {
+                element: RefData.DEAL_LIMITS[i].products[j].element,
+                type: RefData.DEAL_LIMITS[i].products[j].type,
+                productsAdded: RefData.DEAL_LIMITS[i].products[j].productsAdded,
+              },
+              deal_sublimits: {
+                element: RefData.DEAL_SUBLIMITS[i].products[j].element,
+                type: RefData.DEAL_SUBLIMITS[i].products[j].type,
+                productsAdded: RefData.DEAL_SUBLIMITS[i].products[j].productsAdded,
+              },
+            });
+          }
+          break;
+        case 'counterparties':
+          for (let j = 0; j < RefData.LABELS[i].counterparties.length; j += 1) {
+            rowData.push({
+              label: {
+                text: RefData.LABELS[i].counterparties[j].text,
+                type: RefData.LABELS[i].counterparties[j].type,
+              },
+              deal_limits: {
+                element: RefData.DEAL_LIMITS[i].counterparties[j].element,
+                type: RefData.DEAL_LIMITS[i].counterparties[j].type,
+                counterpartyAdded: RefData.DEAL_LIMITS[i].counterparties[j].counterpartyAdded,
+              },
+              deal_sublimits: {
+                element: RefData.DEAL_SUBLIMITS[i].counterparties[j].element,
+                type: RefData.DEAL_SUBLIMITS[i].counterparties[j].type,
+                counterpartyAdded: RefData.DEAL_SUBLIMITS[i].counterparties[j].counterpartyAdded,
+              },
+            });
+          }
+          break;
+        case 'booking':
+          for (let j = 0; j < RefData.LABELS[i].booking.length; j += 1) {
+            rowData.push({
+              label: {
+                text: RefData.LABELS[i].booking[j].text,
+                type: RefData.LABELS[i].booking[j].type,
+              },
+              deal_limits: {
+                element: RefData.DEAL_LIMITS[i].booking[j].element,
+                type: RefData.DEAL_LIMITS[i].booking[j].type,
+              },
+              deal_sublimits: {
+                element: RefData.DEAL_SUBLIMITS[i].booking[j].element,
+                type: RefData.DEAL_SUBLIMITS[i].booking[j].type,
+              },
+            });
+          }
+          break;
+        case 'collateral':
+          for (let j = 0; j < RefData.LABELS[i].collateral.length; j += 1) {
+            rowData.push({
+              label: {
+                text: RefData.LABELS[i].collateral[j].text,
+                type: RefData.LABELS[i].collateral[j].type,
+              },
+              deal_limits: {
+                element: RefData.DEAL_LIMITS[i].collateral[j].element,
+                type: RefData.DEAL_LIMITS[i].collateral[j].type,
+              },
+              deal_sublimits: {
+                element: RefData.DEAL_SUBLIMITS[i].collateral[j].element,
+                type: RefData.DEAL_SUBLIMITS[i].collateral[j].type,
+              },
+            });
+          }
+          break;
+        case 'derisking':
+          for (let j = 0; j < RefData.LABELS[i].derisking.length; j += 1) {
+            rowData.push({
+              label: {
+                text: RefData.LABELS[i].derisking[j].text,
+                type: RefData.LABELS[i].derisking[j].type,
+              },
+              deal_limits: {
+                element: RefData.DEAL_LIMITS[i].derisking[j].element,
+                type: RefData.DEAL_LIMITS[i].derisking[j].type,
+              },
+              deal_sublimits: {
+                element: RefData.DEAL_SUBLIMITS[i].derisking[j].element,
+                type: RefData.DEAL_SUBLIMITS[i].derisking[j].type,
+              },
+            });
+          }
+          break;
+        default:
+          break;
+      }
     }
     return rowData;
   }
