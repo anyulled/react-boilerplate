ebbb474e4bab195b295b17b9ff2957ad89caf447
Mauricio Fernandes <mood@gft.com>
2017-02-02 15:44:44 +0100

    solved merge conflicts

--- app/components/Version4/derisking/ExcludedInvestorsComponent/index.js
+++ app/components/Version4/derisking/ExcludedInvestorsComponent/index.js
@@ -3,29 +3,29 @@
 * ExcludedInvestorsComponent
 *
 */
-
 import React, { PropTypes } from 'react';
-
 import InvestorItem from '../InvestorItem';
-
-function ExcludedInvestorsComponent(props) {
-	if (props.excludedInvestors && props.excludedInvestors.length > 0) {
-		const excludedInvestorsItems = props.excludedInvestors.map((item, key) =>
-			<InvestorItem key={key} name={item.name} />
-		);
-		return (
-			<div className="investor-panel">
-				{excludedInvestorsItems}
-				{props.children}
-			</div>
-		);
-	}
-	return (
-		<div>
-			<p className="text-center">No investors excluded.</p>
-		</div>
-	);
-
+function ExcludedInvestorsComponent({ children, excludedInvestors }) {
+  if ((excludedInvestors) && (excludedInvestors.length > 0)) {
+    const investorsItems = excludedInvestors.map((item, key) =>
+      <InvestorItem key={key} id={item.id} name={item.name} country={item.country} cr={item.cr} sc={item.sc} />
+    );
+    return (
+      <div className="investor-panel">
+        {investorsItems}
+        {children}
+      </div>
+    );
+  }
+  return (
+    <div>
+      <p className="text-center">No investors excluded.</p>
+      {children}
+    </div>
+  );
 }
-
+ExcludedInvestorsComponent.propTypes = {
+  children: PropTypes.object,
+  excludedInvestors: PropTypes.array.isRequired,
+};
 export default ExcludedInvestorsComponent;
