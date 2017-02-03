664eaa80df538cd27e7b1260f6252cf462983a96
Jose Ruiz <Jose-Maria.Ruiz@gft.com>
2017-02-03 10:09:38 +0100

    Added InvestorDetailComponent.

---
+++ app/components/Version4/derisking/InvestorDetailComponent/tests/index.test.js
@@ -0,0 +1,11 @@
+// import InvestorDetailComponent from '../index';
+
+import expect from 'expect';
+// import { shallow } from 'enzyme';
+// import React from 'react';
+
+describe('<InvestorDetailComponent />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
---
+++ app/components/Version4/derisking/fakeData.js
@@ -0,0 +1,21 @@
+export default {
+  InvestorDetail: {
+    general: [
+      { label: 'CRM Responsible', value: 'Peter Mustermann' },
+      { label: 'Credit rating', value: 'iA', colSize: 5 },
+      { label: 'Score card', value: '1101', colSize: 7 },
+      { label: 'Counterparty type', value: 'CORPORATE' },
+      { label: 'Country of domicile', value: 'GERMANY' },
+      { label: 'Industry sector (NACE)', value: 'Industry description' },
+    ],
+    legalEntity: [
+      { label: 'Paragon ID', value: '1234567890', colSize: 5 },
+      { label: 'Name', value: 'SIEMENS AKTIENGESELLSCHAFT', colSize: 7 },
+    ],
+    parent: [
+      { label: 'Paragon ID', value: '1234567890', colSize: 5 },
+      { label: 'Name', value: 'SIEMENS AKTIENGESELLSCHAFT', colSize: 7 },
+      { label: 'Credit rating', value: 'iA' },
+    ],
+  },
+};
